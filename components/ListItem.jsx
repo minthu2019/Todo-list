61098edaed07adb43248e4ba06e2f0a76e766a6b
Min Thu Khaing <mtk.min2003@gmail.com>
2024-08-05 22:52:25 +0630

    first commit

---
+++ components/ListItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button } from '@/components/ui/button';
+
+const ListItem = ({todo, removeTodo, finishTodo}) => {
+  return (
+       <li key={todo.id} className='min-w-[400px]   flex items-center justify-between gap-4'>
+          <p className='min-w-[320px] text-lg p-2 rounded-lg border border-blue-200'>{todo.text}</p>
+         <div className='flex gap-2'>
+            {/* Delete Button */}
+            <Button onClick={()=> removeTodo(todo.id)} variant='secondary' size='lg'>Delete</Button>
+
+            {/* finished Button */}
+            <label htmlFor="my_modal_7" className="btn w-22 bg-blue-200 hover:bg-blue-400 ">Finished</label>
+             {/* Put this part before </body> tag */}
+             <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+                <div className="modal" role="dialog">
+                <div className="modal-box">
+                    <h3 className="text-lg font-bold text-green-500">Success!</h3>
+                    <p className="py-4 text-black">Congratulation! Your Todo is finished.</p>
+                    <div className='flex justify-end'>
+                     <Button className=' text-right' onClick={()=> removeTodo(todo.id)} variant='outline' size='lg'>Finished</Button>
+                    </div>
+                </div>
+                
+            </div>
+         </div>
+        </li>
+  )
+}
+
+export default ListItem
+
---
+++ components/TodoList.jsx
@@ -0,0 +1,76 @@
+"use client"
+import React from 'react'
+import { useState } from 'react';
+
+// import shadcn ui
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import ListItem from './ListItem';
+
+const page = () => {
+  const [count, setCount] = useState(0)
+  const [todos, setTodo] = useState([]);
+  const [todoText, setTodoText] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if(todoText.trim()) {
+        setTodo([
+            ...todos,
+            {
+                id: Math.random(),
+                text: todoText,
+            }
+        ])
+
+        setCount(count + 1)
+       
+        setTodoText("");
+    }
+  };
+
+  const removeTodo = (id) => {
+    setTodo(todos.filter((todo)=>todo.id !== id))
+    setCount(count-1)
+  };
+
+  const finishTodo = (id) => {
+    setTodo(todos.filter((todo)=>todo.id !== id))
+  }
+
+  return (
+      <div className=' flex flex-col gap-2 items-center justify-center mt-10'>
+        <h1 className='flex items-center justify-center gap-6 text-4xl  font-bold text-blue-300 mb-6'>Todo List <span className='ml-3'>{count}</span></h1>
+         
+         {/* todo input form */}
+         <form onSubmit={addTodo} className='w-[400px] mb-5'>
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input 
+            type="text" 
+            placeholder="Enter what you want ..."
+            value={todoText}
+            onChange={(e)=> setTodoText(e.target.value)}
+            />
+            <Button type="submit" variant='outline'>Add</Button>
+          </div>
+         </form>
+        
+        
+
+       
+        {/* border */}
+         <div className='w-[400px] h-[2px] bg-blue-500 mb-10'></div>
+
+        {/* listItem */}
+        <ul className='flex flex-col gap-4'>
+            {todos.map((todo)=> {
+                return (
+                    <ListItem todo={todo} removeTodo={removeTodo} finishTodo={finishTodo}/>
+                )
+            })}
+        </ul>
+      </div>
+  )
+}
+
+export default page
